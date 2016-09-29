512c8c50d8b049cf45c0a398fec4fbdcaa762053
Adhip <adhipvih@buffalo.edu>
2016-09-29 16:30:09 -0400

    Min Stack Implemeted

--- Algorithms/src/MinStack.java
+++ Algorithms/src/MinStack.java
@@ -1,12 +1,32 @@
 import java.util.*;
-public class MinStack<E> {
+public class MinStack {
 
-	Stack<E> regular  = new Stack<E>();
-	Stack<E> min = new Stack<E>();
+	Stack<Integer> regular  = new Stack<Integer>();
+	Stack<Integer> min = new Stack<Integer>();
 	
 	
-	public void push(E data){
+	public void push(int data){
 		regular.push(data);
+		if(min.isEmpty() || min.peek()>data)
+			min.push(data);
+	}
+	
+	public int pop(){
+		if(regular.isEmpty())
+			return Integer.MIN_VALUE;
+		
+		int result = regular.pop();
+		if(min.peek()==result)
+			min.pop();
+		
+		return result;
+	}
+	
+	public int MinStack(){
+		return min.peek();
+	}
+	
+	public static void main(String[] args){
 		
 	}
 }
