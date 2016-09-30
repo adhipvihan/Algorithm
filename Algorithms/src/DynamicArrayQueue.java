53b2636c56e3a74c0facb1c7b2cab1f599a1e016
adhipvih <adhipvih@buffalo.edu>
2016-09-30 17:28:01 -0400

    Dynamic Que Implementation

--- Algorithms/src/DynamicArrayQueue.java
+++ Algorithms/src/DynamicArrayQueue.java
@@ -20,6 +20,44 @@ public class DynamicArrayQueue {
 	}
 	
 	public void expand(){
-		//
+		int len = this.size;
+		int[] temp = new int[len<<1];
+		
+		for(int i=front;i<=rear;i++)
+			temp[i-front] = temp[i%capacity];
+        dataArray = temp;
+        front = 0;
+        rear = size-1;
+		capacity<<=1;
+	
+	}
+	
+	public int Deque(){
+		if(size==0)
+			throw new IllegalStateException("Que Empty");
+		int result = dataArray[front];
+		front = (front+1)%this.capacity;
+		shrink();
+		return result;
+	}
+	
+	public void shrink(){
+		int min = capacity>>2;
+
+		
+		if(size<capacity && size<=min){
+			int[] temp = new int[this.capacity>>1];
+			for(int i=front;i<=rear;i++)
+				temp[i-front] = temp[i%capacity];
+	        dataArray = temp;
+	        front = 0;
+	        rear = size-1;
+			capacity>>=1;
+		}
+			
+	}
+	
+	public static void main(String[] args){
+		
 	}
 }
